645652b9823ce77d11abeface69dd224cfc6346c
albecabrera <albe.cabrera@yahoo.de>
2024-06-20 16:22:21 +0200

    commit -m  "Objekte"

---
+++ src/Objekte/objekte.js
@@ -0,0 +1,25 @@
+
+// let iban = DE8098076543;
+// let bic = "WELADEFFXXX";
+// let kontostand = 3500;
+// let aktiv = true;
+
+// Ein Objekt hat immer Eigencheigenschaften und Werten
+
+let konto_1 = {
+  iban = DE8098076543;
+  bic = "WELADEFFXXX";
+  kontostand = 3500;
+  aktiv = true;
+};
+ 
+console.log(konto_1);
+
+let konto_2 = {
+  iban = DE8098076234;
+  bic = "WELADEFWXXX";
+  kontostand = 4500;
+  aktiv = true;
+};
+ 
+console.log(konto_2);
